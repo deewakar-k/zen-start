2236e98465e6e09411a537b0df2c9d26e9460295
deewakar-k <dakshftw01@gmail.com>
2025-03-27 23:36:04 +0530

    added docker-compose file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default async function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <h1 className="text-2xl font-medium"> welcome to zen app</h1>
